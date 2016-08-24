badd9ffb8968e88fa36c999f8ebcaaa5617ca6ca
Ravindra Kumar <ravidsrk@gmail.com>
2016-08-24 17:04:46 +0530

    Syncing latest files from https://github.com/googlesamples/android-architecture

--- generators/app/sync_templates.js
+++ generators/app/sync_templates.js
@@ -1,25 +1,31 @@
 'use strict';
 
+const renamer = require('renamer');
 const rimraf = require('rimraf');
+const async = require('async');
 const path = require('path');
 const mv = require('mv');
 const mkdirp = require('mkdirp');
-const clone = require("nodegit").Clone;
+const Git = require("nodegit");
 const replace = require("replace");
+const filter = require('filter-files');
+const findInFiles = require('find-in-files');
 const ncp = require('ncp').ncp;
+const Finder = require('fs-finder');
 
-rimraf.sync(path.join(__dirname, '/templates'))
-rimraf.sync(path.join(__dirname, '/tmp'))
-mkdirp('./templates')
+// Clone a given repository into the `./tmp` folder.
+// rimraf.sync(__dirname + '/templates')
+rimraf.sync(__dirname + '/tmp')
+// mkdirp('./templates')
 
-clone("https://github.com/googlesamples/android-architecture", "./tmp")
+Git.Clone("https://github.com/googlesamples/android-architecture", "./tmp")
   .then(function(repo) {
-    // checkOutAndCopy(repo,"todo-mvp")
+    checkOutAndCopy(repo,"todo-mvp")
     // checkOutAndCopy(repo,"todo-mvp-loaders");
     // checkOutAndCopy(repo,"todo-mvp-clean")
-    // checkOutAndCopy(repo,"todo-mvp-dagger")
+    // checkOutAndCopy(repo,"todo-mvp-dagger") 
     // checkOutAndCopy(repo,"todo-mvp-contentproviders")
-    checkOutAndCopy(repo, "todo-databinding");
+    // checkOutAndCopy(repo, "todo-databinding");
   })
   .catch(function(err) {
     console.log(err);
@@ -36,33 +42,37 @@ function checkOutAndCopy(repo, name) {
       replace({
         regex: "com.example.android.architecture.blueprints.todoapp",
         replacement: "<%= appPackage %>",
-        paths: ['./templates/todoapp'],
+        paths: ['./tmp/todoapp'],
         recursive: true,
-        silent: true,
+        silent: false,
       });
 
       mv('./tmp/todoapp/.gitignore', './tmp/todoapp/gitignore', function(err) {
         if (err) {
           return console.log(err);
+        } else {
+          return console.log("Renamed root folder .gitignore")
         }
-        console.log("Renamed root folder .gitignore")
       });
 
       mv('./tmp/todoapp/app/.gitignore', './tmp/todoapp/app/gitignore', function(err) {
         if (err) {
           return console.log(err);
+        } else {
+          return console.log("Renamed app folder .gitignore")
         }
-        console.log("Renamed app folder .gitignore")
       });
 
       console.log("Copying files to ./templates/" + name);
+
       ncp.limit = 1600;
+
       ncp('./tmp/todoapp', './templates/' + name, function(err) {
         if (err) {
           return console.error(err);
+        } else {
+          return console.log('Copying complete!');
         }
-        console.log('Copying complete!');
-        rimraf.sync(path.join(__dirname, '/tmp'))
       });
     });
 }
