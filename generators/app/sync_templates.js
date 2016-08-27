8225397f816600484fcaecfc09465182844e3591
Ravindra Kumar <ravidsrk@gmail.com>
2016-08-27 23:52:43 +0530

    Updated sync template script to work on one go;

--- generators/app/sync_templates.js
+++ generators/app/sync_templates.js
@@ -11,68 +11,91 @@ const filter = require('filter-files');
 const findInFiles = require('find-in-files');
 const ncp = require('ncp').ncp;
 const Finder = require('fs-finder');
+const deferred = require('deferred');
 
 // Clone a given repository into the `./tmp` folder.
 rimraf.sync(__dirname + '/templates')
+rimraf.sync(__dirname + '/tmp')
 mkdirp('./templates')
 
 clone("https://github.com/googlesamples/android-architecture", "./tmp")
   .then(function(repo) {
-    checkOutAndCopy(repo,"todo-mvp");
-    // checkOutAndCopy(repo,"todo-mvp-loaders");
-    // checkOutAndCopy(repo,"todo-mvp-clean");
-    // checkOutAndCopy(repo,"todo-mvp-dagger");
-    // checkOutAndCopy(repo,"todo-mvp-contentproviders");
-    // checkOutAndCopy(repo,"todo-databinding");
+    checkOutAndCopy(repo, "todo-mvp")(function(success) {
+      checkOutAndCopy(repo, "todo-mvp-dagger")(function(success) {
+        checkOutAndCopy(repo, "todo-mvp-loaders")(function(success) {
+          checkOutAndCopy(repo, "todo-mvp-clean")(function(success) {
+            checkOutAndCopy(repo, "todo-mvp-contentproviders")(function(success) {
+              checkOutAndCopy(repo, "todo-databinding")(function(success) {
+                rimraf.sync(__dirname + '/tmp')
+                console.log('success is', success);
+              });
+            });
+          });
+        });
+      });
+    });
   })
   .catch(function(err) {
     console.log(err);
   });
 
 function checkOutAndCopy(repo, name) {
+  let def = deferred();
   repo.getBranch('refs/remotes/origin/' + name)
     .then(function(reference) {
       console.log("Checking out branch " + name);
       return repo.checkoutRef(reference);
     })
     .then(function() {
-
-      replace({
-        regex: "com.example.android.architecture.blueprints.todoapp",
-        replacement: "<%= appPackage %>",
-        paths: ['./tmp/todoapp'],
-        recursive: true,
-        silent: false,
-      });
-
-      mv('./tmp/todoapp/.gitignore', './tmp/todoapp/gitignore', function(err) {
-        if (err) {
-          return console.log(err);
-        } else {
-          return console.log("Renamed root folder .gitignore")
-        }
-      });
-
-      mv('./tmp/todoapp/app/.gitignore', './tmp/todoapp/app/gitignore', function(err) {
-        if (err) {
-          return console.log(err);
-        } else {
-          return console.log("Renamed app folder .gitignore")
-        }
-      });
-
-      console.log("Copying files to ./templates/" + name);
-
-      ncp.limit = 1600;
-
-      ncp('./tmp/todoapp', './templates/' + name, function(err) {
-        if (err) {
-          return console.error(err);
-        } else {
-          console.log('Removing /tmp!');
-          rimraf.sync(__dirname + '/tmp');
-          return console.log('Copying complete!');
-        }
-      });
+      replaceAndRename(true);
+      copyFiles(name, def);
     });
+  return def.promise;
+}
+
+function copyFiles(name, def) {
+  console.log("Copying files to ./templates/" + name);
+
+  ncp('./tmp/todoapp', './templates/' + name, function(err) {
+    if (err) {
+      console.error(err);
+      def.reject('error');
+    } else {
+      replaceAndRename(false);
+      def.resolve('Copying complete!');
+    }
+  });
+}
+
+function replaceAndRename(toggle) {
+  let dotGitignore = "./tmp/todoapp/.gitignore";
+  let gitIgnore = "./tmp/todoapp/gitignore";
+  let appDotGitignore = "./tmp/todoapp/app/.gitignore";
+  let appGitIgnore = "./tmp/todoapp/app/gitignore";
+
+  replace({
+    regex: toggle ? "com.example.android.architecture.blueprints.todoapp" : "<%= appPackage %>",
+    replacement: toggle ? "<%= appPackage %>" : "com.example.android.architecture.blueprints.todoapp",
+    paths: ['./tmp/todoapp'],
+    recursive: true,
+    silent: true,
+  });
+
+  mv(toggle ? dotGitignore : gitIgnore, toggle ? gitIgnore : dotGitignore, function(err) {
+    if (err) {
+      return console.log(err);
+    } else {
+      let file = toggle ?  ".gitignore" : "gitignore";
+      return console.log("Renamed root folder " + file);
+    }
+  });
+
+  mv(toggle ? appDotGitignore : appGitIgnore, toggle ? appGitIgnore : appDotGitignore, function(err) {
+    if (err) {
+      return console.log(err);
+    } else {
+      let file = toggle ?  ".gitignore" : "gitignore";
+      return console.log("Renamed app folder " + file);
+    }
+  });
 }
